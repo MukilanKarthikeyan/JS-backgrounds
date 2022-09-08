52ccd74e0d3bc8d2c29a9421f11d9e4cca145697
Mukilan Karthikeyan <MukilanKarthikeyan@users.noreply.github.com>
2022-09-08 16:10:54 -0400

    initial commit, been working on terrain generation

---
+++ empty-example/example.js
@@ -0,0 +1,12 @@
+//init variables
+
+function setup() {
+    //the setup function runs first
+    //create the canvas work space
+    createCanvas(800, 600, WEBGL);
+
+}
+
+function draw() {
+    //to update the js section
+}
